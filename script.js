c8b0c2d934fe4849574f993ba56c5e6be843df7b
Spew45 <103700395+Spew45@users.noreply.github.com>
2022-05-26 17:28:41 -0700

    changes

--- script.js
+++
@@ -1,9 +0,0 @@
-function changeFavicon(); {
-    var link = document.querySelector("link[rel~='icon']");
-    if (!link) {
-        link = document.createElement('link');
-        link.rel = 'icon';
-        document.getElementsByTagName('head')[0].appendChild(link);
-        link.href = 'dump/favicon.png';
-    }
-}
